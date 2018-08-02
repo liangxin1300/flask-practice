26fe67dabd30a5f7a0cae96f6df513aa1897ebbb
liangxin1300 <XLiang@suse.com>
2018-08-02 14:25:12 +0800

    Chapter 4

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # The bottom import is a workaround to circular imports, 
 # a common problem with Flask applications
-from app import routes
+from app import routes, models
